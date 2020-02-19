a6d6418ddd3fd827fa14cc0f84744e231d8cef67
DwarfCu <DwarfCu@users.noreply.github.com>
2020-02-19 10:50:19 +0100

    Create Kafka installation script

---
+++ Docker/install_kafka.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yum install -y java-1.8.0-openjdk vim
+curl http://apache.rediris.es/kafka/2.4.0/kafka_2.11-2.4.0.tgz -o kafka_2.11-2.4.0.tgz
+tar xzf kafka_2.11-2.4.0.tgz -C /opt/
+ln -s /opt/kafka_2.11-2.4.0/ /opt/kafka
+echo "export KAFKA_HOME=/opt/kafka" >> ~/.bashrc
+echo "export PATH=$PATH:$KAFKA_HOME/bin" >> ~/.bashrc
+source ~/.bashrc
